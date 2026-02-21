5a36c46b7a2da4353e4670beb825f561af0c5593
Vili Hätönen <vili.hato@gmail.com>
2026-02-21 21:05:02 +0100

    predict price per sqm

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 - rent increases
 - define when apartment is rented out (matters for compounding interest)
 - allow renting out and living simultenously/separately
-- support subletting
+- support subletting for the rental scenarios
+- merge market asking price with actuals to find out margin
--- src/analyse/utils.py
+++ src/analyse/utils.py
@@ -1,5 +1,6 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
+import pandas as pd
 
 def get_straight_line_distance(address1, address2, manhattan_distance_multiplier = 1.2):
     """
@@ -23,3 +24,27 @@ def get_straight_line_distance(address1, address2, manhattan_distance_multiplier
         'distance_km': distance,
         'estimated_walking_min': walking_time_min
     }
+
+def wrangle_actuals(df_actuals: pd.DataFrame) -> pd.DataFrame:
+    df_actuals["date"] = pd.to_datetime(df_actuals["Koopdatum"], format="mixed")
+    df_actuals["year_quarter"] = df_actuals["date"].dt.to_period("Q")
+    df_actuals["year_month"] = df_actuals["date"].dt.to_period("M")
+    df_actuals["year_quarter"] = df_actuals["year_quarter"].dt.to_timestamp()
+    df_actuals["year_month"] = df_actuals["year_month"].dt.to_timestamp()
+
+    # rename
+    df_actuals.rename(columns={
+        "Koopsom_EUR": "price",
+        "Koopsom_per_m2_EUR": "price_per_sqm",
+        "Postcode": "postal_code",
+        }, inplace=True
+    )
+    
+    # get floor area
+    df_actuals["floor_area"] = round(df_actuals["price"] / df_actuals["price_per_sqm"], 1)
+    df_actuals["floor_area"] = df_actuals["floor_area"].ffill()
+    df_actuals["price_per_sqm"] = df_actuals["price"] / df_actuals["floor_area"]
+    df_actuals["status"] = "sold"
+    df_actuals.sort_values("date", inplace=True)
+
+    return df_actuals
